3bcd4abcb45c7672d815a02076ea0f8dd56502de
Meghana C V <meghana.cvmcvm@gmail.com>
2024-05-08 23:40:00 +0530

    pages

---
+++ src/routes/task-manager/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let count = 0;
+
+    $: {
+        console.log(count)
+    }
+
+    $: doubled = count*2;
+</script>
+
+<div class="text-white">
+	<div>I am task manager page - {count}</div>
+    <div>Doubled value: {doubled}</div>
+	<button on:click={() => count++}> Increment </button>
+</div>
