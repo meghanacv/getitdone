dc5d47262b2647bc95f604603c6d960ede7975b0
Meghana C V <meghana.cvmcvm@gmail.com>
2024-05-09 15:04:19 +0530

    task item & list

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,15 +1,52 @@
 <script>
-	let count = 0;
+  import TaskList from "../../components/task-manager/TaskList.svelte";
+  const listName1 = "To Do";
 
-    $: {
-        console.log(count)
+  const taskList = [
+    {
+      id: "l-1",
+      text: "List 1",
+      items: [
+        { id: "t-1", text: "Task 1" },
+        { id: "t-2", text: "Task 2" },
+        { id: "t-3", text: "Task 3" }
+      ]
+    },
+    {
+      id: "l-2",
+      text: "List 2",
+      items: [
+        { id: "t-4", text: "Task 4" },
+        { id: "t-5", text: "Task 5" },
+        { id: "t-6", text: "Task 6" }
+      ]
+    },
+    {
+      id: "l-3",
+      text: "List 3",
+      items: [
+        { id: "t-7", text: "Task 7" },
+        { id: "t-8", text: "Task 8" },
+        { id: "t-9", text: "Task 9" }
+      ]
     }
-
-    $: doubled = count*2;
+  ];
 </script>
 
 <div class="text-white">
-	<div>I am task manager page - {count}</div>
-    <div>Doubled value: {doubled}</div>
-	<button on:click={() => count++}> Increment </button>
+  {JSON.stringify(taskList)}
+</div>
+
+<div class="p-10 h-full">
+  <div class="text-white text-2xl mb-6">Some List</div>
+  <button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
+    + Add List
+  </button>
+  <div class="flex-it h-full">
+    <div class="flex-it flex-row rounded-xl h-full">
+      <TaskList listName={listName1} />
+      <TaskList listName={"In Progess" + listName1} />
+      <TaskList listName="Done" />
+    </div>
+  </div>
 </div>
