a812da1b283cd262d072e8986b2ad21935cd7f65
Meghana C V <meghana.cvmcvm@gmail.com>
2024-05-18 23:54:10 +0530

    dispatch event

--- src/components/task-manager/Editable.svelte
+++ src/components/task-manager/Editable.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
   export let value;
   let isEditing = false;
 </script>
@@ -7,7 +9,15 @@
   <div class="flex-it">
     <!--<textarea bind:value={value}/>-->
     <textarea bind:value />
-    <button on:click={() => (isEditing = false)} class="flex underline">Close</button>
+    <button
+      on:click={() => {
+        isEditing = false;
+        dispatch("whatever");
+      }}
+      class="flex underline"
+    >
+      Close</button
+    >
     <div>{value}</div>
   </div>
 {:else}
