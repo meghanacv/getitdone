3c87a7af13ef199a3a2ef7084043733d9cb9b2d0
Meghana C V <meghana.cvmcvm@gmail.com>
2024-05-18 17:57:28 +0530

    bind to component

--- src/components/task-manager/Editable.svelte
+++ src/components/task-manager/Editable.svelte
@@ -1,19 +1,17 @@
 <script>
-    export let defaultValue;
+  export let value;
   let isEditing = false;
-  let value=defaultValue;
 </script>
 
 {#if isEditing}
   <div class="flex-it">
-   <!--<textarea bind:value={value}/>--> 
-    <textarea bind:value/>
-    <button 
-        on:click={() => isEditing = false} 
-        class="flex underline">Close</button>
+    <!--<textarea bind:value={value}/>-->
+    <textarea bind:value />
+    <button on:click={() => (isEditing = false)} class="flex underline">Close</button>
+    <div>{value}</div>
   </div>
 {:else}
-  <button on:click={() => isEditing = true}>
+  <button on:click={() => (isEditing = true)}>
     <slot />
   </button>
 {/if}
