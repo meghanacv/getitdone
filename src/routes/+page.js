8a3b19806a87def2b4c12c0efb36229a385f8f8f
Meghana C V <meghana.cvmcvm@gmail.com>
2024-06-04 23:29:45 +0530

    prerendering

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,10 @@
-export const ssr = false;
+export const prerender = false;
 
 export function load() {
+  const randomNum = Math.floor(Math.random() * 10);
   console.log("Calling load!");
   return {
     test1: "Hello World",
-    test2: 100
+    test2: randomNum
   };
 }
