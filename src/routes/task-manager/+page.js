70239745fe5ab7acdf1114230455f58f36d59bf0
Meghana C V <meghana.cvmcvm@gmail.com>
2024-06-04 16:38:53 +0530

    load function

--- src/routes/task-manager/+page.js
+++ src/routes/task-manager/+page.js
@@ -1,1 +1,11 @@
 export const ssr = false;
+
+export function load() {
+  //Fetch data from API/Database...
+  console.log("Load Calling");
+
+  return {
+    appName: "Task Manager",
+    content: "Board"
+  };
+}
