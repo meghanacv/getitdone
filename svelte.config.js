ddb727af684b24b251d0aabbba230244ef47b394
Meghana C V <meghana.cvmcvm@gmail.com>
2024-06-07 18:16:44 +0530

    fix: adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter({
+      // Specify the Node.js runtime version
+      precompress: false,
+      env: {
+        runtime: "node18" // or 'node16'
+      }
+    })
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
